fffc57235033c7a0f03fcc4a851fb71055c9f56a
unknown <kristiina.sikkar@itcollege.ee>
2016-04-02 16:33:28 +0300

    7. nädala praktikumi 1. ül

---
+++ Kodused harjutused/2. nädal/Galerii/kontroller.php
@@ -0,0 +1,10 @@
+<?php 
+$pildid=array(
+  array("big"=>"img/esimene.jpg", "small"=>"thumb/esimene-thumb.jpg", "alt"=>"Esimene"),
+  array("big"=>"img/teine.jpg", "small"=>"thumb/teine-thumb.jpg", "alt"=>"Teine"),
+  array("big"=>"img/kolmas.jpg", "small"=>"thumb/kolmas-thumb.jpg", "alt"=>"Kolmas"),
+  array("big"=>"img/neljas.jpg", "small"=>"thumb/neljas-thumb.jpg", "alt"=>"Neljas")
+ );
+ 
+ include("index-week5.html");
+?>
