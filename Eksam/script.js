4e5fbb81748f08c4516125f4dcb5fca2108a23a0
unknown <kristiina.sikkar@itcollege.ee>
2016-06-12 14:50:12 +0300

    eksam - parandus

--- Eksam/script.js
+++ Eksam/script.js
@@ -8,10 +8,17 @@ function showLocalTime(){
 	seconds = checkTime(seconds);
 	var timeStamp = hours + ":" + minutes + ":" + seconds;
 
-	document.cookie = "localTime = " + timeStamp;
+
+	document.cookie = "localTime = " + timeStamp + "; expires = ";
 	document.getElementById("localTime").innerHTML = timeStamp;
 	
-	
+	var now = new Date();
+	var time = now.getTime();
+	var expireTime = time + 1000*36000;
+	now.setTime(expireTime);
+	var tempExp = 'Wed, 31 Oct 2012 08:50:17 GMT';
+	document.cookie = "localTime = " + timeStamp + "; expires = "+now.toGMTString()+';path=/';
+	//document.cookie = 'cookie=ok;expires='+now.toGMTString()+';path=/';
 	//var t = setTimeout(showLocalTime, 500);
 }
 
@@ -22,4 +29,3 @@ function checkTime(time){
 	return time;
 }
 	
-
